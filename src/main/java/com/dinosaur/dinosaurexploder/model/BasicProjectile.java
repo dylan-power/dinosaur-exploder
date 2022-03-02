c10bcaecbf48780dc07ebb9fa7120cfd0c755fdf
Maxime Aldahan <aldahan@unistra.fr>
2022-03-02 21:05:48 +0000

    Added moving spaceship and start of background

---
+++ src/main/java/com/dinosaur/dinosaurexploder/model/BasicProjectile.java
@@ -0,0 +1,2 @@
+package com.dinosaur.dinosaurexploder.model;public class BasicProjectile {
+}
---
+++ src/main/java/com/dinosaur/dinosaurexploder/model/Dinosaurs.java
@@ -0,0 +1,6 @@
+package com.dinosaur.dinosaurexploder.model;
+
+public interface Enemy {
+    public void update();
+    public void shoot();
+}
