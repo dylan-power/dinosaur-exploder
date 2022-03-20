4c1a6f8410529edfc6b0de05b688099a5f934dc6
Ian Worley <44382773+IanWorley@users.noreply.github.com>
2022-03-20 13:56:45 -0500

    Clean up imports

--- src/main/java/com/dinosaur/dinosaurexploder/model/GreenDinoComponent.java
+++ src/main/java/com/dinosaur/dinosaurexploder/model/GreenDinoComponent.java
@@ -4,10 +4,8 @@ import com.almasb.fxgl.core.math.Vec2;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.component.Component;
-import com.almasb.fxgl.physics.PhysicsComponent;
 import com.almasb.fxgl.time.LocalTimer;
 import javafx.geometry.Point2D;
-import javafx.scene.image.Image;
 import javafx.util.Duration;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.spawn;
