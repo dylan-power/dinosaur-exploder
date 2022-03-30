fe57b8fe10c772e3a56f766c4dda91e0a261ebb1
VonDei <62793491+jvondermarck@users.noreply.github.com>
2022-03-30 12:31:57 +0100

    Update height and width dimension (-50 for each)

--- src/main/java/com/dinosaur/dinosaurexploder/view/DinosaurGUI.java
+++ src/main/java/com/dinosaur/dinosaurexploder/view/DinosaurGUI.java
@@ -4,8 +4,8 @@ import com.almasb.fxgl.app.GameSettings;
 
 public class DinosaurGUI {
     public void initSettings(GameSettings settings) {
-        settings.setWidth(600);
-        settings.setHeight(800);
+        settings.setWidth(550);
+        settings.setHeight(750);
         settings.setTitle("Dinosaur Exploder");
         settings.setVersion("1.0");
 
