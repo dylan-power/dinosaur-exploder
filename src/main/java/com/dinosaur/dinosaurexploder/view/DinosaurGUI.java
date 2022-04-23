8f11c52f1c9cff565d2daa688dba77496b6d4519
philipgk <philipgk@umich.edu>
2022-04-22 20:59:30 -0400

    Added game menu

--- src/main/java/com/dinosaur/dinosaurexploder/view/DinosaurGUI.java
+++ src/main/java/com/dinosaur/dinosaurexploder/view/DinosaurGUI.java
@@ -1,12 +1,29 @@
 package com.dinosaur.dinosaurexploder.view;
 
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.scene.FXGLMenu;
+import com.almasb.fxgl.app.scene.SceneFactory;
+import javafx.scene.Scene;
 
 public class DinosaurGUI {
+    public static final int WIDTH = 550;
+    public static final int HEIGHT = 750;
+
     public void initSettings(GameSettings settings) {
-        settings.setWidth(550);
-        settings.setHeight(750);
+        settings.setWidth(WIDTH);
+        settings.setHeight(HEIGHT);
         settings.setTitle("Dinosaur Exploder");
+        settings.setMainMenuEnabled(true);
+
+        // Custom main menu
+        settings.setSceneFactory(new SceneFactory() {
+            @Override
+            public FXGLMenu newMainMenu() {
+                return new DinosaurMenu();
+            }
+        });
+
+
         settings.setVersion("1.0");
         settings.setTicksPerSecond(60); // check info : settings.setProfilingEnabled(true);
     }
