c26c00eb1144d47e1a00ec52ab30b444505a16c6
Nadia Pintos Velasco <nadia.pintos@EDCMXENLT0434.local>
2022-07-03 14:53:23 -0500

    added a selector, works with keyboard not when mouse is hovering, when clicked or enter key is used, the button action works

--- src/main/java/com/dinosaur/dinosaurexploder/view/PauseMenu.java
+++ src/main/java/com/dinosaur/dinosaurexploder/view/PauseMenu.java
@@ -2,7 +2,10 @@ package com.dinosaur.dinosaurexploder.view;
 
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
+import javafx.beans.binding.Bindings;
 import javafx.geometry.Pos;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -17,14 +20,14 @@ public class PauseMenu extends FXGLMenu {
         super(MenuType.GAME_MENU);
 
         PauseButton btnBack = new PauseButton("Back",() -> fireResume());
-        PauseButton btnControls = new PauseButton("Controls",() -> {});
+        PauseButton btnControls = new PauseButton("Controls", () -> {});
         PauseButton btnQuitGame = new PauseButton("Quit Game",() -> fireExit());
 
         var bg = new Rectangle(getAppWidth(), getAppHeight(), Color.color(0,0,0,0.5));
         var box = new VBox(15,
                 btnBack,
                 btnControls,
-                new PauseButton("Placeholder 1",() -> {}),
+                new PauseButton("Placeholder 1", () -> {}),
                 new PauseButton("Placeholder 2",() -> {}),
                 btnQuitGame);
 
@@ -40,19 +43,44 @@ public class PauseMenu extends FXGLMenu {
     }
 
     private static class PauseButton extends StackPane {
+
+        private static final Color SELECTED_COLOR = Color.WHITE;
+        private static final Color NOT_SELECTED_COLOR = Color.GRAY;
         private String name;
         private Runnable action;
 
         private  Text text;
+        private Rectangle selector;
         public PauseButton(String name, Runnable action) {
             this.name = name;
             this.action = action;
 
             text = getUIFactoryService().newText(name, Color.WHITE, 24.0);
 
-            setAlignment(Pos.CENTER_LEFT);
+            text.strokeProperty().bind(
+                    Bindings.when(focusedProperty()).then(SELECTED_COLOR).otherwise(NOT_SELECTED_COLOR)
+            );
+            text.setStrokeWidth(0.5);
 
-            getChildren().addAll(text);
+            selector = new Rectangle(5, 22, Color.WHITE);
+            selector.setTranslateX(-20);
+            selector.visibleProperty().bind(focusedProperty());
+
+            setAlignment(Pos.CENTER_LEFT);
+            setFocusTraversable(true);
+
+            setOnKeyPressed(e -> {
+                if (e.getCode() == KeyCode.ENTER) {
+                    action.run();
+                }
+            });
+
+            setOnMouseClicked(event->{
+                action.run();
+            });
+
+
+            getChildren().addAll(selector, text);
 
         }
     }
