8f3c96812c2a7dbaf9854cce314ce826148b6876
Pretham Muthappa <98420696+PrethamMuthappa@users.noreply.github.com>
2024-04-10 10:34:38 +0530

    Made menu changes
    added images

--- src/main/java/com/dinosaur/dinosaurexploder/view/DinosaurMenu.java
+++ src/main/java/com/dinosaur/dinosaurexploder/view/DinosaurMenu.java
@@ -6,14 +6,21 @@ import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.ui.FontType;
 import com.dinosaur.dinosaurexploder.model.GameConstants;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class DinosaurMenu extends FXGLMenu {
 
-    public DinosaurMenu() {
+    public DinosaurMenu() throws FileNotFoundException {
         super(MenuType.MAIN_MENU);
 
         Media media = new Media(getClass().getResource(GameConstants.MAINMENU_SOUND).toExternalForm());
@@ -26,19 +33,30 @@ public class DinosaurMenu extends FXGLMenu {
         var title = FXGL.getUIFactoryService().newText(GameConstants.GAME_NAME, Color.LIME, FontType.MONO, 35);
         var startButton = new Button("Start Game");
         var quitButton = new Button("Quit");
+        FileInputStream fileInputStream= new FileInputStream("../dinosaur-exploder/src/main/resources/assets/textures/dinomenu.png");
+        Image image = new Image(fileInputStream);
 
-        startButton.setMinSize(200, 100);
-        quitButton.setMinSize(200, 100);
+        ImageView imageView = new ImageView(image);
+
+        imageView.setFitHeight(150);
+        imageView.setFitWidth(120);
+        imageView.setX(250);
+        imageView.setY(200);
+
+        imageView.setPreserveRatio(true);
+
+        startButton.setMinSize(50, 50);
+        quitButton.setMinSize(140, 50);
 
         title.setTranslateY(100);
-        title.setTranslateX(getAppWidth() / 2 - 175);
+        title.setTranslateX(getAppWidth() / 2 - 145);
 
-        startButton.setTranslateY(350);
-        startButton.setTranslateX(getAppWidth() / 2 - 100);
+        startButton.setTranslateY(400);
+        startButton.setTranslateX(getAppWidth() / 2 - 50);
         startButton.setStyle("-fx-font-size:20");
 
-        quitButton.setTranslateY(490);
-        quitButton.setTranslateX(getAppWidth() / 2 - 100);
+        quitButton.setTranslateY(500);
+        quitButton.setTranslateX(getAppWidth() / 2 - 50);
         quitButton.setStyle("-fx-font-size:20");
 
         startButton.setOnAction(event -> {
@@ -48,7 +66,7 @@ public class DinosaurMenu extends FXGLMenu {
         quitButton.setOnAction(event -> fireExit());
 
         getContentRoot().getChildren().addAll(
-                bg, title, startButton, quitButton
+                bg, title, startButton, quitButton,imageView
         );
     }
 
